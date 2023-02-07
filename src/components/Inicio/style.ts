0f01962c3b567f2f6bae7b397ac56635140a5171
ElmoJr <elmojuniorduarte@gmail.com>
2023-02-06 21:04:45 -0300

    fix JobTitle @media size proportions

--- src/components/Inicio/style.ts
+++ src/components/Inicio/style.ts
@@ -12,7 +12,21 @@ export const Container = styled.div`
   height: 100vh;
   margin: auto;
 
+  p {
+    font-size: 20px;
+    width: 220px;
+    height: 35px;
+    line-height: 35px;
+  }
+
   @media (max-width: 830px) {
     width: 265px;
+
+    p {
+      font-size: 14px;
+      width: 170px;
+      height: 27px;
+      line-height: 27px;
+    }
   }
 `;
