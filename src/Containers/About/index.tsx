ae03e4f26ca648b9cc8b8fcb32425c9995584105
ElmoJr <elmojuniorduarte@gmail.com>
2023-05-30 23:07:32 -0300

    refactor aboutme

--- src/Containers/About/index.tsx
+++ src/Containers/About/index.tsx
@@ -1,28 +1,38 @@
 import "./styles.css";
-
-// import ProfilePiture from "../../components/ProfilePiture";
-// import SectionTitle from "../../components/SectionTitle";
-// import Text from "../../components/Text";
-// import { Button, Container, AboutInfo } from "./style";
+import photo from "../../assets/Foto.png";
 
 export default function About() {
-  return <div>About</div>;
-  // return (
-  // <Container>
-  //   <ProfilePiture />
-  //   <AboutInfo>
-  //     <SectionTitle text="Sobre mim:" />
-
-  //     <Text
-  //       text="Oi meu nome é Elmo Junior, estou cursando ciências da
-  //           computação na UNESA, sempre gostei de tecnologia e me apaixonei
-  //           por desenvolvimento web e frontend, então decidi  continuar por
-  //           esse caminho. ||
-  //           Desenvolvi alguns projetos utilizando as principais linguagens e
-  //           tecnologias para desenvolvimento web."
-  //     />
-  //     <Button>Baixar CV</Button>
-  //   </AboutInfo>
-  // </Container>
-  // );
+  return (
+    <div id="about" className="about-container section-padding">
+      <img src={photo} alt="Uma foto minha" />
+      <div className="about-info-container">
+        <h2>
+          Sobre mim<span className="yellow">:</span>
+        </h2>
+        <p>
+          Sou estudante de Ciência da Computação na UNESA. Tenho um grande
+          interesse pela tecnologia, especialmente pelo desenvolvimento web e
+          frontend. Essa paixão me impulsionou a seguir esse caminho e aprimorar
+          minhas habilidades.
+          <br />
+          <br />
+          Durante minha jornada, tive a oportunidade de desenvolver vários
+          projetos utilizando as principais linguagens e tecnologias para a web.
+          Essas experiências me deram um conhecimento prático sólido,
+          permitindo-me explorar diferentes abordagens e encontrar soluções
+          criativas para desafios complexos.
+          <br />
+          <br />
+          Estou sempre em busca de novas oportunidades para expandir meu
+          conhecimento e aprimorar minhas habilidades. Sou entusiasta e adoro
+          aprender e aplicar as últimas tendências e melhores práticas do setor.
+        </p>
+        <div className="about-link-container">
+          <a href="#">Baixar CV</a>
+          <a href="https://www.linkedin.com/in/elmojr/">LinkedIn</a>
+          <a href="https://github.com/oElmoJr">GitHub</a>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/Containers/Header/index.tsx
+++ src/Containers/Header/index.tsx
@@ -2,7 +2,7 @@ import "./styles.css";
 
 export default function Header() {
   return (
-    <div className="header-container">
+    <div id="home" className="header-container">
       <div className="header-title-container">
         <h1>
           Oi<span className="yellow">,</span> eu sou o ElmoJr
