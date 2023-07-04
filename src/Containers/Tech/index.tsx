ec76ca2d69dca30daa8788aadf7e4fa28bb3feb4
ElmoJr <elmojuniorduarte@gmail.com>
2023-07-04 11:41:50 -0300

    Finalize technologies section

--- src/Containers/Tech/index.tsx
+++ src/Containers/Tech/index.tsx
@@ -1,23 +1,14 @@
-import { A11y, Autoplay } from "swiper";
+import React from "react";
+
+import * as Popover from "@radix-ui/react-popover";
+import "./styles.css";
+
 import { Swiper, SwiperSlide } from "swiper/react";
-
-import vscode from "./assets/vscode-original 1.svg";
-import html from "./assets/html5-original 1.svg";
-import css from "./assets/css3-original 1.svg";
-import js from "./assets/javascript-original 1.svg";
-import ts from "./assets/typescript-original 1.svg";
-import react from "./assets/react-original 2.svg";
-import figma from "./assets/figma-original 1.svg";
-import canva from "./assets/canva-original 1.svg";
-import git from "./assets/git-original 1.svg";
-import github from "./assets/github-original 1.svg";
-import bootstrap from "./assets/bootstrap-original 1.svg";
-import tailwind from "./assets/tailwindcss-plain 1.svg";
-import wordpress from "./assets/wordpress-plain 1.svg";
-
-import Title from "../../components/Title";
-
+import { A11y, Autoplay } from "swiper";
 import "swiper/css";
+
+import { icons } from "./icons";
+import Title from "../../components/Title";
 import "./styles.css";
 
 export default function Tech() {
@@ -26,102 +17,47 @@ export default function Tech() {
       <Title>Tecnologias que uso:</Title>
 
       <Swiper
-        modules={[Autoplay, A11y]}
-        spaceBetween={10}
-        loop={true}
-        speed={2000}
-        slidesPerView={4}
-        breakpoints={{
-          // when window width is >= 768px
-          768: {
-            width: 768,
-            slidesPerView: 8,
-          },
-        }}
-        autoplay={{
-          delay: 0,
-          disableOnInteraction: false,
-        }}
         className="mySwiper"
+        modules={[Autoplay, A11y]}
+        slidesPerView={4.5}
+        spaceBetween={5}
+        speed={3000}
+        loop={true}
+        preventClicks={false}
+        autoplay={{ delay: 0, disableOnInteraction: false }}
+        breakpoints={{ 768: { width: 768, slidesPerView: 8 } }}
       >
-        <SwiperSlide>
-          <img src={vscode} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={html} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={css} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={js} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={ts} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={react} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={figma} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={canva} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={git} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={github} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={bootstrap} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={tailwind} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={wordpress} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={vscode} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={html} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={css} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={js} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={ts} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={react} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={figma} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={canva} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={git} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={github} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={bootstrap} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={tailwind} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={wordpress} alt="" />
-        </SwiperSlide>
+        {
+          /* repeat 2x */
+          [1, 2].map((repetiçao, i) => (
+            <React.Fragment key={repetiçao + i}>
+              {
+                /* icons */
+                icons.map((e) => (
+                  <SwiperSlide key={e.name + i}>
+                    <Popover.Root>
+                      <Popover.Trigger>
+                        <img src={e.img} alt={`${e.name}: ${e.desc}`} />
+                      </Popover.Trigger>
+                      <Popover.Portal>
+                        <Popover.Content
+                          className="PopoverContent"
+                          sideOffset={5}
+                        >
+                          <h3>
+                            {e.name}
+                            <span className="yellow">:</span>
+                          </h3>
+                          {e.desc}
+                        </Popover.Content>
+                      </Popover.Portal>
+                    </Popover.Root>
+                  </SwiperSlide>
+                ))
+              }
+            </React.Fragment>
+          ))
+        }
       </Swiper>
     </div>
   );
