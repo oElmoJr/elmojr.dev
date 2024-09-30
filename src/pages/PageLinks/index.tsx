ff954504a9dfb16ab1961cd880b9b7e6edaff8ed
ElmoJr <72280546+oElmoJr@users.noreply.github.com>
2024-09-29 21:14:09 -0300

    update social links

--- src/pages/PageLinks/index.tsx
+++ src/pages/PageLinks/index.tsx
@@ -13,6 +13,9 @@ import {
   goodreads,
   spotify,
   pinterest,
+  elmobot,
+  bluesky,
+  threads
 } from "./inports";
 
 import profilePhoto from "../../assets/SquarePhoto.png";
@@ -70,43 +73,57 @@ export default function PageLinks() {
       
       <div className="pagelinks-links-container">
         <div className="pagelinks-socialLinks-container">
-          <a target="_blank" href="https://www.linkedin.com/in/elmojr/">
-            <img src={linkedin} alt="Twiter icon" />
-          </a>
-
-
-          <a target="_blank" href="https://instagram.com/_elmojr">
-            <img src={instagram} alt="Instagram icon" />
-          </a>
-
-          <a target="_blank" href="https://twitter.com/_ElmoJr">
-            <img src={twitter} alt="Twitter icon" />
+          <a target="_blank" href="https://t.me/oElmoJr">
+            <img src={telegram} alt="Telegram icon" />
           </a>
 
           <a target="_blank" href="https://github.com/oElmoJr">
             <img src={github} alt="GitHub icon" />
           </a>
 
-
-          <a target="_blank" href="https://t.me/oElmoJr">
-            <img src={telegram} alt="Telegram icon" />
+          <a target="_blank" href="https://www.linkedin.com/in/elmojr/">
+            <img src={linkedin} alt="Twiter icon" />
           </a>
 
 
+
+          <a target="_blank" href="https://instagram.com/_elmojr">
+            <img src={instagram} alt="Instagram icon" />
+          </a>
+
+          <a target="_blank" href="https://www.threads.net/@_elmojr">
+            <img src={threads} alt="Threads icon" />
+          </a>
+
           <a target="_blank" href="mailto:elmojrdv@gmail.com">
             <img src={email} alt="Email icon" />
+          </a>
+
+          <a target="_blank" href="https://bsky.app/profile/elmojr.dev">
+            <img src={bluesky} alt="BlueSky icon" />
+          </a>
+
+          <a target="_blank" href="https://pin.it/5XZ5RrboO">
+            <img src={pinterest} alt="pinterest icon" />
           </a>
 
           <a target="_blank" href="https://open.spotify.com/user/elmojunior35?si=7bbb377b42794199">
             <img src={spotify} alt="spotify icon" />
           </a>
           
-          <a target="_blank" href="https://pin.it/5XZ5RrboO">
-            <img src={pinterest} alt="pinterest icon" />
+          
+          <a target="_blank" href="https://wa.me/558498470691?text=Oi,%20o%20que%20voce%20pode%20fazer?">
+            <img src={elmobot} alt="ElmoBot icon" />
           </a>
+
           <a target="_blank" href="https://www.goodreads.com/_elmojr">
             <img src={goodreads} alt="goodreads icon" />
           </a>
+          
+          <a target="_blank" href="https://twitter.com/_ElmoJr">
+            <img src={twitter} alt="Twitter icon" />
+          </a>
+
         </div>
       </div>
 
--- src/pages/PageLinks/inports.ts
+++ src/pages/PageLinks/inports.ts
@@ -1,11 +1,14 @@
-import linkedin from "../../assets/social_Icons/linkedin.png";
-import github from "../../assets/social_Icons/github.png";
-import email from "../../assets/social_Icons/email.png";
-import telegram from "../../assets/social_Icons/telegram.png";
-import instagram from "../../assets/social_Icons/instagram.png";
-import twitter from "../../assets/social_Icons/twitter.png";
-import goodreads from "../../assets/social_Icons/goodreads.png";
-import spotify from "../../assets/social_Icons/spotify.png";
-import pinterest from "../../assets/social_Icons/pinterest.png";
+import linkedin from "../../assets/social_Icons/linkedin.svg";
+import github from "../../assets/social_Icons/github.svg";
+import email from "../../assets/social_Icons/email.svg";
+import telegram from "../../assets/social_Icons/telegram.svg";
+import instagram from "../../assets/social_Icons/instagram.svg";
+import twitter from "../../assets/social_Icons/twitter.svg";
+import goodreads from "../../assets/social_Icons/goodreads.svg";
+import spotify from "../../assets/social_Icons/spotify.svg";
+import pinterest from "../../assets/social_Icons/pinterest.svg";
+import elmobot from "../../assets/social_Icons/elmobot.svg";
+import bluesky from "../../assets/social_Icons/bluesky.svg";
+import threads from "../../assets/social_Icons/threads.svg";
 
-export { linkedin, github, email, telegram, instagram, twitter, goodreads, spotify, pinterest };
+export { linkedin, github, email, telegram, instagram, twitter, goodreads, spotify, pinterest, elmobot, bluesky, threads };
